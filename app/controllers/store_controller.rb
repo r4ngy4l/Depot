36a87b09ba67ac204d8cb8e46a5385229808f868
unknown <robert.angyal@.infostyle.net>
2010-04-15 23:09:22 +0200

    Checkout

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -42,6 +42,28 @@ class StoreController < ApplicationController
     end
   end
 
+  def checkout
+    @hide_buttons = true
+    @cart = find_cart
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty" )
+    else
+      @order = Order.new
+    end
+  end
+
+  def save_order
+    @cart = find_cart
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order" )
+    else
+      render :action => 'checkout'
+    end
+  end
+
 private
 
   def find_cart
