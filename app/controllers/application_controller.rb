9e53140742006b0bf1cde5e82b983e0e4d4da829
unknown <robert.angyal@.infostyle.net>
2010-04-24 00:39:27 +0200

    admin authentication who_bought controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,25 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  layout "store"
+  before_filter :authorize, :except => :login
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  protected
+  def authorize
+    flash[:notice] = "Please create an admin user" if User.count == 0
+    unless User.find_by_id(session[:user_id]) || User.count == 0
+      session[:original_uri] = request.request_uri
+      flash[:notice] = "Please log in"
+      redirect_to :controller => 'admin' , :action => 'login'
+    end
+    if (User.count == 0 && request.path_parameters['action'] != "add_user")
+      flash[:notice] = "Please create an account."
+      redirect_to(:controller=>"login", :action=>"add_user")
+    end
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,9 @@
 class StoreController < ApplicationController
+  before_filter :find_cart, :except => :empty_cart
+
+  def authorize
+  end
+
   def index
     @products = Product.find_products_for_sale
     @cart = find_cart
@@ -64,10 +69,10 @@ class StoreController < ApplicationController
     end
   end
 
-private
+  private
 
   def find_cart
-    session[:cart] ||= Cart.new
+    @cart = (session[:cart] ||= Cart.new)
   end
 
   def redirect_to_index(msg = nil)
